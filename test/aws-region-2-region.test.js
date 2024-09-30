1ec2c82d249a84ab35b7f5153b39503e7360875d
gpa21 <gpa21@sfu.ca>
2024-09-30 15:46:21 -0400

    initial commit

---
+++ test/aws-region-2-region.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsRegion2Region = require('../lib/aws-region-2-region-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-region-2-region-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsRegion2Region.AwsRegion2RegionStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
